a87c5106b89f9d4bdb6ada1a15f549054b23ae4e
ACondori95 <97042212+ACondori95@users.noreply.github.com>
2023-01-28 13:58:42 -0300

    directives

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
-  styleUrls: ['./document-list.component.css']
+  styleUrls: ['./document-list.component.css'],
 })
-export class DocumentListComponent {
+export class DocumentListComponent implements OnInit {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
+  documents: Document[] = [
+    new Document('1', 'doc-1', 'This is the first document', 'localhost', []),
+    new Document('2', 'doc-2', 'This is the second document', 'localhost', []),
+    new Document('3', 'doc-3', 'This is the third document', 'localhost', []),
+    new Document('4', 'doc-4', 'This is the fourth document', 'localhost', []),
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+  }
 }
